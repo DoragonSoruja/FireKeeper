f622583b270c8503052082520490929e168f44e6
Sebastian Roberts <tiansebas09@gmail.com>
2019-12-11 17:51:55 -0600

    Hiding Token

---
+++ FireKeeper/Program.cs
@@ -0,0 +1,36 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace FireKeeper
+{
+    class Program
+    {
+        public DiscordSocketClient Client;
+        public CommandHandler Handler;
+
+        static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+
+        private async Task Start()
+        {
+            Client = new DiscordSocketClient();
+
+            Handler = new CommandHandler();
+
+            await Client.LoginAsync(Discord.TokenType.Bot, "Token", true);
+
+            await Client.StartAsync();
+
+            await Handler.Install(Client);
+
+            Client.Ready += Client_Ready;
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_Ready()
+        {
+            Console.WriteLine("Bot ready!");
+            return;
+        }
+    }
+}
